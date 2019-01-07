63def6c8b3aa8efc0c56388394844cf305521f9e
BuiNguyenHoangAnh <hoanganhbui099@gmail.com>
2019-01-07 14:14:54 +0700

    add card to ColorPage

--- src/pages/ColorPage.js
+++ src/pages/ColorPage.js
@@ -10,7 +10,7 @@ const ColorPage = () => {
         breadcrumbs={[{ name: 'typography', active: true }]}>
         <Row>
             <Col>
-                <Card>
+                <Card style={{backgroundColor: '#1abc9c' }}>
                     <CardHeader>TORQUOISE</CardHeader>
                     <CardBody>
                         <Typography>#1abc9c</Typography>
@@ -21,7 +21,7 @@ const ColorPage = () => {
             </Col>
 
             <Col>
-                <Card>
+                <Card style={{backgroundColor: '#2ecc71' }}>
                     <CardHeader>EMERALD</CardHeader>
                     <CardBody>
                         <Typography>#2ecc71</Typography>
@@ -32,7 +32,7 @@ const ColorPage = () => {
             </Col>
 
             <Col>
-                <Card>
+                <Card style={{backgroundColor: '#607d8b' }}>
                     <CardHeader>BLUE GRAY</CardHeader>
                     <CardBody>
                         <Typography>#607d8b</Typography>
@@ -43,12 +43,104 @@ const ColorPage = () => {
             </Col>
 
             <Col>
-                <Card>
+                <Card style={{backgroundColor: '#ff5722' }}>
                     <CardHeader>DEEP ORANGE</CardHeader>
                     <CardBody>
                         <Typography>#ff5722</Typography>
                         <Typography>rgb(255, 87, 34)</Typography>
                         <Typography>hsl(14, 100%, 57%)</Typography>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+
+        <Row>
+            <Col>
+                <Card style={{backgroundColor: '#cddc39' }}>
+                    <CardHeader>LIME</CardHeader>
+                    <CardBody>
+                        <Typography>#cddc39</Typography>
+                        <Typography>rgb(205, 220, 57)</Typography>
+                        <Typography>hsl(66, 70%, 54%)</Typography>
+                    </CardBody>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card style={{backgroundColor: '#a2c0cc' }}>
+                    <CardHeader>#A2C0CC</CardHeader>
+                    <CardBody>
+                        <Typography>#a2c0cc</Typography>
+                        <Typography>rgb(162, 192, 204)</Typography>
+                        <Typography>hsl(197, 29%, 72%)</Typography>
+                    </CardBody>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card style={{backgroundColor: '#009688' }}>
+                    <CardHeader>TEAL</CardHeader>
+                    <CardBody>
+                        <Typography>#009688</Typography>
+                        <Typography>rgb(0, 150, 136)</Typography>
+                        <Typography>hsl(174, 100%, 29%)</Typography>
+                    </CardBody>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card style={{backgroundColor: '#bfbebe' }}>
+                    <CardHeader>#BFBEBE</CardHeader>
+                    <CardBody>
+                        <Typography>#bfbebe</Typography>
+                        <Typography>rgb(255, 235, 59)</Typography>
+                        <Typography>hsl(54, 100%, 62%)</Typography>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+
+        <Row>
+            <Col>
+                <Card style={{backgroundColor: '#fba4b4' }}>
+                    <CardHeader>#FBA4B4</CardHeader>
+                    <CardBody>
+                        <Typography>#fba4b4</Typography>
+                        <Typography>rgb(251, 164, 180)</Typography>
+                        <Typography>hsl(349, 92%, 81%)</Typography>
+                    </CardBody>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card style={{backgroundColor: '#ffd54f' }}>
+                    <CardHeader>#FFD54F</CardHeader>
+                    <CardBody>
+                        <Typography>#ffd54f</Typography>
+                        <Typography>rgb(255, 213, 79)</Typography>
+                        <Typography>hsl(46, 100%, 65%)</Typography>
+                    </CardBody>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card style={{backgroundColor: '#ff5e00' }}>
+                    <CardHeader>#FF5E00</CardHeader>
+                    <CardBody>
+                        <Typography>#ff5e00</Typography>
+                        <Typography>rgb(255, 94, 0)</Typography>
+                        <Typography>hsl(22, 100%, 50%)</Typography>
+                    </CardBody>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card style={{backgroundColor: '#ffeb3b' }}>
+                    <CardHeader>YELLOW</CardHeader>
+                    <CardBody>
+                        <Typography>#ffeb3b</Typography>
+                        <Typography>rgb(249, 249, 249)</Typography>
+                        <Typography>hsl(0, 0%, 98%)</Typography>
                     </CardBody>
                 </Card>
             </Col>
